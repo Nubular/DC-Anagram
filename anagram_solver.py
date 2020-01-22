6c16e98a85dcd3934e0a62d410eaa40ddcf16cfe
Nubular <nubular2001@gmail.com>
2020-01-22 13:09:19 +0530

    Much more efficient anagram

--- anagram_solver.py
+++ anagram_solver.py
@@ -1,57 +1,40 @@
 import itertools
 import sys
-from stuff import word_dict
+from word_set import word_set
 import asyncio
-
-
-async def find_possible(lst):
-
-    returned_list = []
-    for subset in itertools.permutations(lst,len(lst)):
-        possible = ''
-        for letter in subset:
-            possible += letter
-        if len(possible) == len(lst):
-            returned_list.append(possible)
  
-    return returned_list
-
-def binary_search_string(word,lst):
-    lower = 0
-    upper = len(lst) - 1
-    while(lower<=upper):
-        mid = (lower + upper)//2
-        midpoint = lst[mid]
-        if midpoint > word:
-            upper = mid - 1
-        elif midpoint < word:
-            lower = mid + 1
-        elif lst[mid] == word:
-            return True
-    return False
-
-async def return_words(lst, word_set):
-
+async def return_words(freq):
     returned_list = []
-
-    for word in lst:
-        if binary_search_string(word,word_dict[len(word)]) or  binary_search_string(word.capitalize(),word_dict[len(word)]):
-            # Some words are capitalized in the word_set
-            returned_list.append(word)
-
+    for string in word_set:
+        word_freq = [0]*26
+        flag=True
+        for char in string:
+            if(char>='a' and char <='z'):
+                word_freq[ord(char)-ord('a')]+=1
+            elif(char>='A' and char <='Z'):
+                word_freq[ord(char)-ord('A')]+=1
+            else:
+                flag=False
+                break
+        if word_freq==freq and flag:
+            returned_list.append(string)
     return returned_list
 
 
 async def find_words(word):
 
-    anagram_lst = []
+
     anagram = word
+    freq = [0]*26
     
     for char in anagram:
-        anagram_lst.append(char)
+        if(char>='a' and char <='z'):
+            freq[ord(char)-ord('a')]+=1
+        else:
+            freq[ord(char)-ord('A')]+=1
 
-    possible_words = await find_possible(anagram_lst)
-    actual_words = await return_words(possible_words, word_dict)
+
+    actual_words = await return_words(freq)
     answer = []
     if len(actual_words) == 0:
         print('None found')
@@ -63,3 +46,11 @@ async def find_words(word):
             answer.append(item)
         return answer
 
+async def main():
+    words = await find_words("aergon")
+    for string in words:
+        print(string)
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
